6885ca9505f8ce4e19b51df69adde81fffdec4fb
Megan Yu <megan2521@gmail.com>
2020-06-18 14:11:40 -0700

    Added FileManagement Example

---
+++ FileManagementExample/FileManagementExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FileManagementExample
+//
+//  Created by Megan Yu on 6/18/20.
+//  Copyright © 2020 Megan Yu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // most file operations are done with the NSFileManager class
+        NSFileManager *fileManager = [[NSFileManager alloc] init];
+        [fileManager ]
+    }
+    return 0;
+}
